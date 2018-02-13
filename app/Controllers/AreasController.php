5603a7ebe8ab292b1cb3c521e21554b48f096646
Elias <Elias@DESKTOP-VVQP9VR>
2018-02-13 16:48:27 -0300

    Api Area by get Token

--- app/Controllers/AreasController.php
+++ app/Controllers/AreasController.php
@@ -9,46 +9,59 @@ class AreasController extends BaseController
     public function index($request)
     {
         session_start();
+        
+        $aRequest = (array) $request;
+        $this->setPageTitle('Area');
+        $model = Container::getModel("Area");
+        
+        /* Api Areas */
+        /* URL de Cosumo:
+         * http://localhost:8080/area?api=true
+         * http://localhost:8080/area?api=true&id=1&nombre=Procesos y Tecnologia
+         * */
+        
+        if($aRequest['api'] === 'true' && $aRequest['method'] == 'GET')
+        {
+            if($aRequest['token'])
+            {
+                //Verifica se a aplicacao esta registrada
+                $app  = $model->CheckToken($aRequest['token']);
+                
+                if($app)
+                {
+                    $this->view->area = $model->select();
+                    
+                    if(count($aRequest) > 3)
+                    {
+                        $i = 0;
+                        foreach ($aRequest as $key => $value)
+                        {
+                            if($key == 'api' || $key == 'method' || $key == 'token')
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                $aDados[$i]['field'] = $key;
+                                $aDados[$i]['value'] = $value;
+                            }
+                            $i ++;
+                        }
+                        
+                        $return = $model->SearchAPI($aDados);
+                        die(print_r(json_encode($return), true));
+                    }
+                    die(print_r(json_encode($this->view->area), true));
+                }
+            }
+        }
+        
         if(!isset($_SESSION['access_token']))
         {
             header('Location: /');
         }
         
-        $this->setPageTitle('Area');
-        $model = Container::getModel("Area");
         $this->view->area = $model->select();
-        
-        /* Api Areas */
-        /* URL de Cosumo:  
-         * http://localhost:8080/area?api=true 
-         * http://localhost:8080/area?api=true&id=1&nombre=Procesos y Tecnologia
-         * */
-        
-        $aRequest = (array) $request;
-        if($aRequest['api'] === 'true'&& $aRequest['method'] == 'GET')
-        {
-            if(count($aRequest) > 2)
-            {
-                $i = 0;
-                foreach ($aRequest as $key => $value)
-                {
-                    if($key == 'api' || $key == 'method')
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        $aDados[$i]['field'] = $key;
-                        $aDados[$i]['value'] = $value;
-                    }
-                    $i ++;
-                }
-                
-                $return = $model->SearchAPI($aDados);
-                die(print_r(json_encode($return), true));
-            }
-            die(print_r(json_encode($this->view->area), true));
-        }
         
         /* Render View Paises */
         $this->renderView('area/index', 'layout_idvirtual');
--- app/Models/Area.php
+++ app/Models/Area.php
@@ -131,4 +131,15 @@ class Area extends BaseModel
         
         return $result;
     }
+    
+    public function CheckToken($token)
+    {
+        $sql  = "";
+        $sql .= "SELECT * FROM token WHERE access_token = '" . $token. "'";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->rowCount();
+        $stmt->closeCursor();
+        return $result;
+    }
 }
